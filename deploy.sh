3c2c9c39a94cbf708f404fff035292e65cacd219
kfalia-f <kfalia-f@217-67-187-58.in-addr.mastertelecom.ru>
2019-03-01 11:51:11 -0500

    dep

--- deploy.sh
+++ deploy.sh
@@ -13,11 +13,14 @@ cp /root/nginx.conf /etc/nginx/
 cp /root/default.conf /etc/nginx/conf.d/
 cp /root/animate-custom.css /usr/share/nginx/html/
 cp /root/index.html /usr/share/nginx/html/
+cp /root/index1.html /usr/share/nginx/html/
+cp /root/index2.html /usr/share/nginx/html/
+cp /root/index3.html /usr/share/nginx/html/
 cp /root/style.css /usr/share/nginx/html/
 cp /root/ifcfg-enp0s3 /etc/sysconfig/network-scripts/
 
 sh /root/iptables.sh
 
 rm -rf rs1
-rm -rf nginx.conf animate-custom.css index.html style.css ifcfg-enp0s3
+rm -rf nginx.conf animate-custom.css index.html index1.html index2.html index3.html style.css ifcfg-enp0s3
 reboot
